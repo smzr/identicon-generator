ec5a68741bf1fcd748b2ed2e8ec442c72cca65c8
smzr <smzr@pm.me>
2019-02-15 17:53:14 +0000

    Add: user can define cols and width
    Updated font also

--- main.js
+++ main.js
@@ -1,14 +1,26 @@
 const canvas = document.getElementById("identicon");
 const ctx = canvas.getContext('2d');
-const size = canvas.width;
-const res = size / 6;
+const width = canvas.width;
+const height = canvas.height;
+const padding = height > width ? width / 10 : height / 10 ; // uses the smallest side to get padding
+const shapeWidth = width - (2 * padding); // width after padding is applied
+const shapeHeight = height - (2 * padding); // width after padding is applied
+let colsInput = document.getElementById("cols").value;
+let rowsInput = document.getElementById("rows").value;
+let cols = colsInput;
+let rows = rowsInput;
+let colWidth = Math.round(shapeWidth / cols);
+let rowWidth = Math.round(shapeHeight / rows);
+
+const container = document.getElementById("container");
+container.style.width = canvas.width+"px"; // set the width of container to width of canvas so that canvas is centered
 
 function randomColor() {
     const goldenRatio = 0.618033988749895;
     let hue = Math.random();
-    hue += goldenRatio;
+    hue += goldenRatio; // using golden ratio as spacing makes the values more evenly distributed
     hue %= 1;
-    let c = HSVtoRGB(hue, 0.5, 0.95);
+    let c = HSVtoRGB(hue, 0.5, 0.95); // converts to rgb so it can be used in js
     return "rgb("+c.r+", "+c.g+", "+c.b+")"
 }
 
@@ -47,30 +59,43 @@ function new2DArr(cols, rows, value) {
 }
 
 function generateShape() {
-    for (let x = 0; x < 3; x++) {
-        for (let y = 0; y < 5; y++) {
+    init();
+    for (let x = 0; x < Math.round(cols / 2); x++) {
+        for (let y = 0; y < rows; y++) {
             grid[x][y] = (Math.random() > 0.5) + 0;
         }
     }
-    for (let x = 3; x < 5; x++) {
-        for (let y = 0; y < 5; y++) {
-            grid[x][y] = grid[2 - (x - 2)][y]
+    for (let x = Math.round(cols / 2); x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
+            grid[x][y] = grid[Math.round(cols / 2) - (x - Math.floor(cols / 2)) - 1][y]
         }
     }
 }
 
 function draw() {
-    for (let x = 0; x < 5; x++) {
-        for (let y = 0; y < 5; y++) {
+    for (let x = 0; x < cols; x++) {
+        for (let y = 0; y < rows; y++) {
             if (grid[x][y]) {
                 ctx.fillStyle = color
             }
             else {
                 ctx.fillStyle = 'rgb(238, 238, 238)'
             }
-            ctx.fillRect((x * res) + (res / 2), (y * res) + (res / 2), res, res);
+            ctx.fillRect((x * colWidth) + padding, (y * rowWidth) + padding, colWidth, rowWidth);
         }
     }
+}
+
+function init() {
+    ctx.fillStyle = 'rgb(238, 238, 238)';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    colsInput = document.getElementById("cols").value;
+    rowsInput = document.getElementById("rows").value;
+    cols = colsInput;
+    rows = rowsInput;
+    colWidth = Math.round(shapeWidth / cols);
+    rowWidth = Math.round(shapeHeight / rows);
+    grid = new2DArr(cols, rows, 0);
 }
 
 function generateImg() {
@@ -89,8 +114,12 @@ function refreshShape() {
     draw();
 }
 
-let grid = new2DArr(5, 5, 0);
-ctx.fillStyle = 'rgb(238, 238, 238)';
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+function updateLabels() {
+    document.getElementById('rowsCounter').innerText = document.getElementById("rows").value;
+    document.getElementById('colsCounter').innerText = document.getElementById("cols").value;
+}
+
+let grid;
 let color;
+updateLabels();
 generateImg();
